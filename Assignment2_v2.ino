e27ee715374290a09f713afb130fe2d05510763b
karim3000 <99026937+karim3000@users.noreply.github.com>
2022-03-10 19:08:48 +0000

    Update Assignment2_v2.ino

--- Assignment2_v2.ino
+++ Assignment2_v2.ino
@@ -3,6 +3,7 @@ int pulse1 = 0.05 * 1000;  //Represents the duration of the Signal 1 pulse
 int led1 = 21;
 ///Task 2 variables
 int button1 = 22; //Pin of Button 1
+unsigned int buttonState
 ///Task 3 variables
 int task3pin = 14;
 ///task 4 variables
@@ -12,15 +13,19 @@ int ANLGinput = 0;
 unsigned int task5avg;
 int readcounter = 0;
 int oldANLG = 0;
+int compAvg = 0;
 ///Task 6 variables
 int task6counter = 0;
+///Task 7 variables
+unsigned int error;
+///Task 8 variables 
+int led2 = 15;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(led1, OUTPUT); //Defines the directionality of the led 1 pin to output
-  //pinMode(led2, OUTPUT); //Defines the directionality of the led 2 pin to output
+  pinMode(led2, OUTPUT); //Defines the directionality of the led 2 pin to output
   pinMode(button1, INPUT); //Defines the directionality of the button 1 pin to input
-  //pinMode(button2, INPUT); //Defines the directionality of the button 2 pin to input
   pinMode(task3pin, INPUT);
   pinMode(task4pin, INPUT);
  
@@ -37,24 +42,36 @@ void task1(){
   delayMicroseconds(pulse1);            //with duration of 50uS to LED 2 representing signal B
   digitalWrite(led1, LOW);}
 void task2(){
-  Serial.write(button1);}
+  buttonState = Serial.read(button1);}
 void task3(){
   
 }
 void task4(){
   readcounter++;
   oldANLG = ANLGinput;
-  ANLGinput = digitalRead(task4pin);
-  Serial.println(ANLGinput);}
+  ANLGinput = digitalRead(task4pin);}
 
 void task5(){
   if (readcounter <= 4){
     task5avg = task5avg + ANLGinput;}
   else {
     task5avg = task5avg + ANLGinput - oldANLG;}
+    compAvg = task5avg / 4;
 }
 void task6(){
   while (task6counter <= 1000){
     __asm__ __volatile__ ("nop");
-  }
+    task6counter++;}
+}
+void task7(){
+  if (compAvg > (0.5 * 256)){
+    error = 1;}
+else{
+    error = 0;}
+}
+void task8(){
+  digitalWrite(led2, error);
+}
+void task9(){
+  
 }
