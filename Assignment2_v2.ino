9871b069c58a3fee974027285df69c66c6564abd
karim3000 <99026937+karim3000@users.noreply.github.com>
2022-03-10 13:36:22 +0000

    Update Assignment2_v2.ino

--- Assignment2_v2.ino
+++ Assignment2_v2.ino
@@ -1,18 +1,24 @@
 ///Task 1 variables
+int pulse1 = 0.05 * 1000;  //Represents the duration of the Signal 1 pulse
 int led1 = 21;
 ///Task 2 variables
 int button1 = 22; //Pin of Button 1
-
-int task3pin = 13;
-
-int pulse1 = 0.05 * 1000;  //Represents the duration of the Signal 1 pulse
-
+///Task 3 variables
+int task3pin = 14;
+///task 4 variables
+int task4pin = 13;
+unsigned int ANLGinput;
+///Task 5 variables
+unsigned int task5avg;
+int readcounter = 0;
 void setup() {
   // put your setup code here, to run once:
   pinMode(led1, OUTPUT); //Defines the directionality of the led 1 pin to output
-  pinMode(led2, OUTPUT); //Defines the directionality of the led 2 pin to output
+  //pinMode(led2, OUTPUT); //Defines the directionality of the led 2 pin to output
   pinMode(button1, INPUT); //Defines the directionality of the button 1 pin to input
-  pinMode(button2, INPUT); //Defines the directionality of the button 2 pin to input
+  //pinMode(button2, INPUT); //Defines the directionality of the button 2 pin to input
+  pinMode(task3pin, INPUT);
+  pinMode(task4pin, INPUT);
  
 }
 
@@ -23,13 +29,21 @@ void loop() {
 }
 
 void task1(){
-  digitalWrite(led2, HIGH); //This line coupled with the 2 below will run a signal 
+  digitalWrite(led1, HIGH); //This line coupled with the 2 below will run a signal 
   delayMicroseconds(pulse1);            //with duration of 50uS to LED 2 representing signal B
-  digitalWrite(led2, LOW);}
+  digitalWrite(led1, LOW);}
 void task2(){
   Serial.write(button1);}
 void task3(){
   
 }
 void task4(){
+  ANLGinput = digitalRead(task4pin);
+  Serial.println(ANLGinput);
+  if (counter >= 4){
+    
+  }
+  task5avg = task5avg +}
+void task5(){
   
+}
